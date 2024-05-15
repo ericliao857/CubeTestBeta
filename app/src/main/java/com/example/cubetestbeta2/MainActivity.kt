0a261c06e4a26a5814369afa1f7c53690ffa5b6c
ericliao <asd844312@gmail.com>
2024-05-15 15:38:36 +0800

    Feat: Toolbar

--- app/src/main/java/com/example/cubetestbeta2/MainActivity.kt
+++ app/src/main/java/com/example/cubetestbeta2/MainActivity.kt
@@ -2,13 +2,16 @@ package com.example.cubetestbeta2
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.findNavController
 import com.example.cubetestbeta2.R
 import com.example.cubetestbeta2.databinding.ActivityMainBinding
 import com.example.cubetestbeta2.vm.MainViewModel
@@ -18,6 +21,9 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModel()
+    private val navController by lazy {
+        findNavController(R.id.nav_host_fragment)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +35,27 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setNavigation()
+        setEventClick()
+    }
+
+    private fun setEventClick() {
+        // 返回
+        binding.ibBack.setOnClickListener {
+            navController.navigateUp()
+        }
+    }
+
+    private fun setNavigation() {
+        navController.addOnDestinationChangedListener { _, destination, bundle ->
+            binding.ibMenu.visibility = if (destination.id == R.id.listFragment) View.VISIBLE else View.INVISIBLE
+            binding.ibBack.visibility = if (destination.id != R.id.listFragment) View.VISIBLE else View.INVISIBLE
+            binding.tvTitle.text = destination.label
         }
     }
 }
